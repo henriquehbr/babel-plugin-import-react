22332d2341b52718baee613976bdaca2bcec18e4
Henrique Borges <henriqueborgeshbr@gmail.com>
2019-10-10 16:21:10 -0300

    fix: move all hooks to the Window interface

---
+++ react-hooks.d.ts
@@ -0,0 +1,25 @@
+interface Window {
+	React: any
+	useState: Function
+	useEffect: Function
+	useContext: Function
+	useReducer: Function
+	useCallback: Function
+	useMemo: Function
+	useRef: Function
+	useImperativeHandle: Function
+	useLayoutEffect: Function
+	useDebugValue: Function
+}
+
+declare const React: any
+declare const useState: Function
+declare const useEffect: Function
+declare const useContext: Function
+declare const useReducer: Function
+declare const useCallback: Function
+declare const useMemo: Function
+declare const useRef: Function
+declare const useImperativeHandle: Function
+declare const useLayoutEffect: Function
+declare const useDebugValue: Function
